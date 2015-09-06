2c1ea1ae9a8e87f24ecb6c23215fa85b524cee06
vishalshubham <vishal.shubham@gmail.com>
2015-09-06 07:29:28 -0400

    Final commit with Push notification properly working

--- app/src/main/java/com/myweather/MainActivity.java
+++ app/src/main/java/com/myweather/MainActivity.java
@@ -15,10 +15,15 @@
  */
 package com.myweather;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.media.Image;
+import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -26,6 +31,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -73,6 +79,26 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
 
             @Override
             protected void onPostExecute(Void aVoid) {
+                NotificationManager NM=(NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notify=new Notification(R.drawable.ic_push,"Pushed Notification",System.currentTimeMillis()+10000);
+                PendingIntent pending=PendingIntent.getActivity(getApplicationContext(), 0, new Intent(),0);
+                notify.setLatestEventInfo(getApplicationContext(), "Pushed", "Notification details",pending);
+                NM.notify(0, notify);
+
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
+
+                mBuilder.setContentIntent(pending).setContentTitle("").setContentText("");
+                mBuilder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+                mBuilder.setLights(Color.RED, 3000, 3000);
+
+
+                Uri notificationsound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+                mBuilder.setSound(notificationsound);
+
+
+
+
                 Intent i = new Intent(MainActivity.this, ThingsActivity.class);
                 SharedPreferences prefs = getSharedPreferences("PREFS", MODE_PRIVATE);
                 String str = prefs.getString("WHOLE_LINE", "");
